2acf55d92321457a89929d760e8d955780d8f16e
ngimdock <ngimdockzemfack@gmail.com>
2023-04-07 20:23:06 +0100

    Some improvements

---
+++ data/mockData.js
@@ -0,0 +1,57 @@
+export const AUTHORS = [
+  {
+    id: 1,
+    name: "",
+  },
+  {
+    id: 2,
+    name: "",
+  },
+  {
+    id: 3,
+    name: "",
+  },
+];
+
+export const BOOKS = [
+  {
+    id: 1,
+    name: "Harry Potter and the Chamber of Secrets",
+    authorId: 1,
+  },
+  {
+    id: 2,
+    name: "Jurassic Park",
+    authorId: 2,
+  },
+  {
+    id: 3,
+    name: "The Hobbit",
+    authorId: 3,
+  },
+  {
+    id: 4,
+    name: "The Lord of the Rings",
+    authorId: 3,
+  },
+  {
+    id: 5,
+    name: "The Silmarillion",
+    authorId: 3,
+  },
+  {
+    id: 6,
+    name: "The Hitchhiker's Guide to the Galaxy",
+    authorId: 2,
+  },
+  {
+    id: 7,
+    name: "The Restaurant at the End of the Universe",
+    authorId: 2,
+  },
+  {
+    id: 8,
+    name: "Life, the Universe and Everything",
+    authorId: 2,
+  },
+];
--- tmp.js
+++ tmp.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import {
-  GraphQLInt,
-  GraphQLList,
-  GraphQLNonNull,
-  GraphQLObjectType,
   GraphQLSchema,
+  GraphQLObjectType,
   GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
 } from "graphql";
+import { AUTHORS, BOOKS } from "./data/mockData.js";
 
+// Construct a schema, using GraphQL schema language
 let users = [
   {
     id: 1,
@@ -38,15 +40,12 @@ let cars = [
   },
 ];
 
-const app = express();
-
 const CarType = new GraphQLObjectType({
   name: "Car",
-  description: "This represent a car for a user.",
+  description: "This represent a car for and user",
   fields: () => ({
     id: { type: new GraphQLNonNull(GraphQLInt) },
     brand: { type: new GraphQLNonNull(GraphQLString) },
-    speed: { type: GraphQLInt },
     ownerId: { type: new GraphQLNonNull(GraphQLInt) },
     owner: {
       type: UserType,
@@ -57,67 +56,68 @@ const CarType = new GraphQLObjectType({
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  description: "This represent a user.",
+  description: "This reprensent a user",
   fields: () => ({
     id: { type: new GraphQLNonNull(GraphQLInt) },
     name: { type: new GraphQLNonNull(GraphQLString) },
     cars: {
       type: new GraphQLList(CarType),
-      resolve: (user) => cars.filter((car) => car.ownerId === user.id),
+      resolve: (currentUser) =>
+        cars.filter((car) => car.ownerId === currentUser.id),
     },
   }),
 });
 
 const RootQueryType = new GraphQLObjectType({
   name: "Query",
-  description: "Root query",
+  description: "Root Query",
   fields: () => ({
-    cars: {
-      type: new GraphQLList(CarType),
-      description: "A list of cars",
-      resolve: () => cars,
-    },
-
     car: {
       type: CarType,
-      description: "Single car",
+      description: "A single car",
       args: {
-        id: { type: new GraphQLNonNull(GraphQLInt) },
+        id: { type: GraphQLInt },
       },
-      resolve: (_, args) => cars.find((car) => car.id === args.id),
+      resolve: (_, { id: carId }) => cars.find((car) => car.id === carId),
     },
 
-    users: {
-      type: new GraphQLList(UserType),
-      description: "A list of users.",
-      resolve: () => users,
+    cars: {
+      type: new GraphQLList(CarType),
+      description: "List of cars",
+      resolve: () => cars,
     },
 
     user: {
       type: UserType,
-      description: "Single user.",
+      description: "A single user",
       args: {
-        id: { type: new GraphQLNonNull(GraphQLInt) },
+        id: { type: GraphQLInt },
       },
-      resolve: (_, args) => users.find((user) => user.id === args.id),
+      resolve: (_, args) => users.find(({ id }) => id === args.id),
+    },
+
+    users: {
+      type: new GraphQLList(UserType),
+      description: "List of users",
+      resolve: () => users,
     },
   }),
 });
 
-const RootMutation = new GraphQLObjectType({
+const RootMutationType = new GraphQLObjectType({
   name: "Mutation",
   description: "Root mutation",
   fields: () => ({
-    createCar: {
+    addCar: {
       type: CarType,
-      description: "Create a car.",
+      description: "Add a car",
       args: {
         brand: { type: new GraphQLNonNull(GraphQLString) },
-        speed: { type: GraphQLInt },
         ownerId: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve: (_, args) => {
         const newCar = { id: cars.length + 1, ...args };
+
         cars.push(newCar);
 
         return newCar;
@@ -126,30 +126,56 @@ const RootMutation = new GraphQLObjectType({
 
     createUser: {
       type: UserType,
-      description: "Create a user.",
+      description: "Create a new user",
       args: {
         name: { type: new GraphQLNonNull(GraphQLString) },
       },
       resolve: (_, args) => {
         const newUser = { id: users.length + 1, ...args };
+
         users.push(newUser);
 
         return newUser;
       },
     },
+
+    removeUser: {
+      type: new GraphQLList(UserType),
+      description: "Remove a user",
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+
+      resolve: (_, { id }) => {
+        const newUsers = users.filter((user) => user.id !== id);
+
+        users = newUsers;
+
+        return users;
+      },
+    },
   }),
 });
 
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
 const schema = new GraphQLSchema({
   query: RootQueryType,
-  mutation: RootMutation,
+  mutation: RootMutationType,
 });
 
+const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
     graphiql: true,
-    schema,
+    schema: schema,
+    // rootValue: root,
   })
 );
 
